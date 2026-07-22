ce9fed17b4b7bb925271d9f24d925243d531976e
prathisha16 <105776318+prathisha16@users.noreply.github.com>
2026-07-22 16:50:18 +0530

    Update outputs.tf

--- terraform/createControlServer/outputs.tf
+++ terraform/createControlServer/outputs.tf
@@ -1,11 +1,7 @@
 output "control_server_public_ip" {
-
   value = aws_instance.control_server.public_ip
-
 }
 
 output "control_server_instance_id" {
-
   value = aws_instance.control_server.id
-
 }
