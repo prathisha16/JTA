eef85b2c3db3f6eb1d3d23619fcb546b86cbad85
prathisha16 <105776318+prathisha16@users.noreply.github.com>
2026-07-22 20:36:06 +0530

    Create control.sh

---
+++ terraform/createControlServer/userdata/control.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install -y ansible git unzip
+
+echo "Ansible Installed Successfully" > /home/ubuntu/setup_complete.txt
+
+ansible --version > /home/ubuntu/ansible_version.txt
