956380bce03a5a15e9c15d834ae8bb696140fbc6
prathisha16 <105776318+prathisha16@users.noreply.github.com>
2026-07-22 21:51:24 +0530

    Update variables.tf

--- terraform/createRemoteServers/variables.tf
+++ terraform/createRemoteServers/variables.tf
@@ -1,1 +1,17 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "ami_id" {
+  description = "Ubuntu AMI"
+  type        = string
+  default     = "ami-052355af2a014bd2c"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t2.micro"
+}
