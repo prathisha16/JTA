b868c9efc6e6a05a9ab0dc34e1ec2b9aa689cba7
prathisha16 <105776318+prathisha16@users.noreply.github.com>
2026-07-22 21:52:30 +0530

    Update outputs.tf

--- terraform/createRemoteServers/outputs.tf
+++ terraform/createRemoteServers/outputs.tf
@@ -1,1 +1,15 @@
+output "web_server1_public_ip" {
+  value = aws_instance.web_server_1.public_ip
+}
 
+output "web_server2_public_ip" {
+  value = aws_instance.web_server_2.public_ip
+}
+
+output "web_server1_private_ip" {
+  value = aws_instance.web_server_1.private_ip
+}
+
+output "web_server2_private_ip" {
+  value = aws_instance.web_server_2.private_ip
+}
