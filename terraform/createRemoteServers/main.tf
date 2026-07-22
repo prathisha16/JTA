7bb412fdcb9533ccd049e023df0d5e097cf1fd4d
prathisha16 <105776318+prathisha16@users.noreply.github.com>
2026-07-22 21:48:02 +0530

    Update main.tf

--- terraform/createRemoteServers/main.tf
+++ terraform/createRemoteServers/main.tf
@@ -1,1 +1,24 @@
+resource "aws_key_pair" "project_key" {
+  key_name   = "project_key"
+  public_key = file("/var/lib/jenkins/project_key.pub")
+}
 
+resource "aws_instance" "web_server_1" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.project_key.key_name
+
+  tags = {
+    Name = "Web-Server-1"
+  }
+}
+
+resource "aws_instance" "web_server_2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.project_key.key_name
+
+  tags = {
+    Name = "Web-Server-2"
+  }
+}
