c3473f840785c36c7e3b20907c3300bf29cf3407
prathisha16 <105776318+prathisha16@users.noreply.github.com>
2026-07-22 16:44:52 +0530

    Update main.tf

--- terraform/createControlServer/main.tf
+++ terraform/createControlServer/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "control_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
-  key_name = var.key_name
+  key_name = aws_key_pair.project_key.key_name
 
   tags = {
     Name = "Control-Server"
