b50ce001d9beb2c09aa4e3e8f5b241f2484e7816
prathisha16 <105776318+prathisha16@users.noreply.github.com>
2026-07-22 20:52:21 +0530

    Update control.sh

--- terraform/createControlServer/userdata/control.sh
+++ terraform/createControlServer/userdata/control.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-apt-get update -y
-apt-get install -y ansible git unzip
+set -eux
 
+apt-get update -y
+apt-get install -y ansible-core git unzip
+
+ansible --version > /home/ubuntu/ansible_version.txt
 echo "Ansible Installed Successfully" > /home/ubuntu/setup_complete.txt
