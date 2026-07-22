084824cf42a4b147eca3d740d74ab161a2d4b33e
prathisha16 <105776318+prathisha16@users.noreply.github.com>
2026-07-22 22:22:38 +0530

    Update main.tf

--- terraform/createRemoteServers/main.tf
+++ terraform/createRemoteServers/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web_server_1" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
-  key_name = data.aws_key_pair.project_key.key_name
+  key_name = aws_key_pair.project_key.key_name
 
   tags = {
     Name = "Web-Server-1"
